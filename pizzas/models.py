5490a838ed4d0adc195070bf397548e4d64dac08
Jack OQuinn <jack_oquinn1@baylor.edu>
2023-08-09 12:46:39 -0500

    12:46 8.9

--- pizzas/models.py
+++ pizzas/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Pizza(models.Model):
     pizza_name = models.CharField(max_length=100)
-    #image_name = models.CharField(max_length=100)
+    image_name_with_file_extension = models.CharField(max_length=100, default='')
 
     def __str__(self):
         return self.pizza_name
