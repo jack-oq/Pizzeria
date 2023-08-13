71330d892f96338d21adebcad4e580704b0e99e0
Jack OQuinn <jack_oquinn1@baylor.edu>
2023-08-13 14:50:30 -0500

    final commit

--- pizzas/forms.py
+++ pizzas/forms.py
@@ -3,7 +3,6 @@ from .models import *
 
 class CommentForm(forms.ModelForm):
     class Meta:
-        model = Pizza
-        fields = ['pizza_name']
-        labels = {'pizza_name':''}
-        
+        model = Comment
+        fields = ['content']
+                
--- pizzas/models.py
+++ pizzas/models.py
@@ -19,3 +19,11 @@ class Topping(models.Model):
 
     def __str__(self):
         return self.topping_name
+
+class Comment(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    content = models.TextField(max_length=300)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.pizza.name} - {self.time}"
